91ecb9d1f068213a2e9f8847b2bd40a4873c7424
Mathieu Saubin <saubinm@free.fr>
2019-06-26 16:47:37 +0200

    Frontend setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { StripeProvider, Elements } from "react-stripe-elements";
+import Shop from "./Shop";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StripeProvider apiKey="pk_test_BT5vh9BIRdTxfPMubPv0xumE">
+      <Elements>
+        <Shop />
+      </Elements>
+    </StripeProvider>
   );
-}
+};
 
 export default App;
