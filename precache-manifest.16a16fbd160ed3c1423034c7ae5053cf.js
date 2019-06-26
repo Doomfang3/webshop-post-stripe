36a056014c5d9121ee516d602f0e237ce02e6532
Mathieu Saubin <saubinm@free.fr>
2019-06-26 17:00:28 +0200

    Updates

---
+++ precache-manifest.16a16fbd160ed3c1423034c7ae5053cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10282646351335d30d93219fb96d8c2a",
+    "url": "/webshop-post-stripe/index.html"
+  },
+  {
+    "revision": "49c78889811f760ccd92",
+    "url": "/webshop-post-stripe/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d730c3b3677e9c6210c4",
+    "url": "/webshop-post-stripe/static/js/2.32538b4f.chunk.js"
+  },
+  {
+    "revision": "49c78889811f760ccd92",
+    "url": "/webshop-post-stripe/static/js/main.13252e24.chunk.js"
+  },
+  {
+    "revision": "9da4ca18e8bf1080d1df",
+    "url": "/webshop-post-stripe/static/js/runtime~main.17a726b2.js"
+  }
+]);
